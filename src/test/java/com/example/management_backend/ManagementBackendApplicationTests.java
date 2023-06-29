9927987c5be40bf342c3f4d656bf83869713bdaa
Sirenology <928489976@qq.com>
2023-06-29 09:20:04 +0800

    提交文件

---
+++ src/test/java/com/example/management_backend/ManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
